2e31df98b3e3da206acd91cddcb7c1a24515daa3
monica <monicamina200@gmail.com>
2025-01-10 02:44:50 +0200

    finished radio screen

---
+++ lib/presentation/screens/home/tabs/radio/Clicked_Reciters.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c13_offline/core/resources/assets_manager.dart';
+
+class ClickedReciters extends StatelessWidget {
+  const ClickedReciters({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetImages.islamiLogo),
+        Padding(
+          padding: const EdgeInsets.only(left: 20,right: 20),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xff202020B2)),
+                  child: Center(
+                    child: Text(
+                      'Radio',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xffE2BE7F)),
+                  width: double.infinity,
+                  height: 40,
+                  child: Center(
+                    child: Text(
+                      'Reciters',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              )],
+          ),
+        )
+        ,
+      ],
+    );
+  }
+}
